c48b40dfed1bac34bada776f8c3a1cfefd066c22
carlos.arroyoh <carlos.arroyoh@uanl.mx>
2022-06-10 10:22:16 -0500

    prier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesModule } from './Heroes/Heroes.module';
+
+import { contadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
